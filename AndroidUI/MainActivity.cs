6d26824488af918b3cc30fe93597d71f8addddf4
B-O-S-S- <b.o.s.s.-base@arcor.de>
2017-08-03 21:52:36 +0200

    Created AndroidUI project with a rudimentary app showing a fixed picture.

---
+++ AndroidUI/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace AndroidUI
+{
+    [Activity(Label = "QR Code Diag", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+            ImageView qrCodeImageView = FindViewById<ImageView>(Resource.Id.QRCodeImageViewID);
+            qrCodeImageView.SetImageResource(Resource.Drawable.anfang);
+        }
+    }
+}
+
