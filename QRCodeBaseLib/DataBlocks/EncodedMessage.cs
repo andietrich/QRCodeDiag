36facc9e529a7bfc65f99325fe1a8065d196305e
BOSS D <boss.dietrich@gmx.de>
2021-03-28 03:45:10 +0200

    add EncodedMessage stub

---
+++ QRCodeBaseLib/DataBlocks/EncodedMessage.cs
@@ -0,0 +1,19 @@
+﻿using QRCodeBaseLib.DataBlocks.SymbolCodes;
+using QRCodeBaseLib.DataBlocks.Symbols;
+using QRCodeBaseLib.DataBlocks.Symbols.EncodingSymbols;
+using QRCodeBaseLib.MetaInfo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QRCodeBaseLib.DataBlocks
+{
+    class EncodedMessage
+    {
+        private CodeSymbolCode<MessageModeSymbol> messageModeSymbol;
+        private CodeSymbolCode<CharCountIndicatorSymbol> charCountIndicator;
+        private CodeSymbolCode<CodeSymbol> encodedMessage;
+
+        // public EncodedMessage
+    }
+}
